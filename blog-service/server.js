e5a7f33c1ebea2f35eee746ef942a5044788b34e
lobyte2 <l.uribe@duocuc.cl>
2025-11-24 03:42:21 -0300

    modificando blog

--- blog-service/server.js
+++ blog-service/server.js
@@ -1,19 +1,20 @@
 import express from 'express';
 import cors from 'cors';
-import rutasUser from './routes/user.routes.js';
+import rutasBlog from './routes/blog.routes.js';
 
 const app = express();
-const PORT = process.env.PORT || 3003;
+// USO CORRECTO DEL PUERTO EN RENDER
+const PORT = process.env.PORT || 3005; // Usamos puerto dinámico
 
 app.use(cors({
-    origin: '*',
+    origin: '*', // CRÍTICO: Permitir comunicación cruzada
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
 app.use(express.json());
 
-// Montamos TODAS las rutas de usuarios bajo el prefijo /users
-app.use('/users', rutasUser);
+// Montamos TODAS las rutas del blog bajo el prefijo /blog
+app.use('/blog', rutasBlog);
 
 app.listen(PORT, () => {
-    console.log(`Servicio de Usuarios corriendo en el puerto ${PORT}`);
+    console.log(`Servicio de Blog corriendo en el puerto ${PORT}`);
 });
